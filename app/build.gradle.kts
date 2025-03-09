21369e6e039dc2bb7b52fe0597d60d2f24ce5ced
Josh <reimer.josh.2006@gmail.com>
2025-03-09 14:09:45 -0300

    added updated ui from public site with new icon assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.josh.teampicker"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 4
         versionName = "1.1"
 
